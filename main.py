179268edd4e2e6abb925ba4d1af7155e57218baf
Bhaskara-B <142556824+Bhaskara-B@users.noreply.github.com>
2024-05-27 08:03:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -84,5 +84,5 @@ def Com_Math():
 # def About():
 #     return render_template("about.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+#if __name__ == "__main__":
+#    app.run(debug=True)
